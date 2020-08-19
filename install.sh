0fe0520f39f7f002f521058652bedc788a7895e5
Philipp Nopper <kontakt@nopper-entwicklung.de>
2020-08-19 08:14:43 +0200

    Add openssh to install list

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #install i3-gaps
-pacman -S xorg-server xorg-xinit xorg-xinput xf86-input-libinput acpi i3-gaps i3blocks i3lock dmenu zsh the_silver_searcher feh flameshot lsd xfce4-terminal firefox alsa-utils weechat gvim php ctags --noconfirm
+pacman -S xorg-server xorg-xinit xorg-xinput xf86-input-libinput acpi i3-gaps i3blocks i3lock dmenu zsh the_silver_searcher feh flameshot lsd xfce4-terminal firefox alsa-utils weechat gvim php ctags openssh --noconfirm
 pacman -S compton --noconfirm
 
 #Install oh-my-zsh
